e6a9bf2c13d0f8cead9d03b61a052aeb4560e543
Palesa <palesa.masiteng@gmail.com>
2023-11-19 17:15:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App() {
+  let [searched, setSearched] = useState("");
+  let [result, setResult] = useState({});
+  let [loaded, setLoaded] = useState(false);
+
+  function allVariables(response) {
+    setLoaded(true);
+    setResult({
+      temperature: response.data.main.temp,
+      wind: Math.round(response.data.wind.speed),
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "b9d9369451dab612d4df9595a347fdbb";
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${apiKey}&units=metric`;
+    axios.get(api).then(allVariables);
+  }
+
+  function updateCity(event) {
+    setSearched(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
   );
+  if (loaded) {
+    return (
+      <div>
+        <h1>Weather App</h1>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(result.temperature)}°C</li>
+          <li>Description: {result.description}</li>
+          <li>Humidity: {result.humidity}%</li>
+          <li>Wind: {result.wind}km/h</li>
+          <li>
+            <img src={result.icon} alt={result.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>Weather App</h1>
+        {form}
+      </div>
+    );
+  }
 }
-
-export default App;
